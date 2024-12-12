2938e06b195e050049932e84510a79aac449ca7d
rondininja <phrondini@gmail.com>
2024-12-12 00:11:06 -0300

    criação da segunda tela da aplicação, para capturar o preço do combustivel

--- app/src/main/java/com/example/fuelcalculator/PriceActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/PriceActivity.kt
@@ -7,23 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
-class MainActivity : AppCompatActivity() {
+class PriceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_price)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.price)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnStart = findViewById<Button>(R.id.btnStart)
-
-        btnStart.setOnClickListener {
-            val intent = Intent(this, PriceActivity::class.java)
-            startActivity(intent)
-        }
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val tiPrice = findViewById<TextInputEditText>(R.id.tiPrice)
     }
 }
